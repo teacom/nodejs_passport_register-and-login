42b21dc3bea84050cf21d017441e728fd1ed2daa
chandan kumar <rockchandan.94@gmail.com>
2021-06-19 11:09:17 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = "mongodb+srv://'your_user_id':'your_password'@cluster_name_here.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+module.exports = {
+    mongoURI: dbPassword
+};
